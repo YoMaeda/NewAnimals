2eafb5f862ac98c279ae88ddc1f09549c8df53eb
前田庸 <yomaeda@maedaisaonoMacBook-Pro.local>
2017-09-09 21:21:06 +0900

    Initial Commit

---
+++ Animals/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Animals
+//
+//  Created by 前田庸 on 2017/09/09.
+//  Copyright © 2017年 Yo Maeda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
